049c7dde67c00e4deaa456f8270fd6e73ddf3855
sikandarx <sikandarhb@gmail.com>
2023-07-26 13:46:10 +0500

    Added Change pfp Function

--- student_profile.php
+++ student_profile.php
@@ -280,9 +280,9 @@ unlink($targetFile); // Delete the existing file
 
 // Move the uploaded file to the target directory
 if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-    echo "<p class='p1 m-5 text-white bg-success text-center' >Your Profile Picture has been changed successfully.</p>";
+    echo "<p class='p1 my-5 text-white bg-success text-center' style='margin-right: auto; margin-left: auto;'>Your Profile Picture has been changed successfully.</p>";
 } else {
-    echo "<p class='p-1 m-5 text-white bg-danger text-center' >Sorry, there was an error changing your Profile Picture!!</p>";
+    echo "<p class='p-1 my-5 text-white bg-danger text-center' >Sorry, there was an error changing your Profile Picture!!</p>";
 }
 }
 }
